faf8b94e8b3cef7b6043847878f6469f36919f57
Abbah <anohabbah@gmail.com>
2020-12-03 23:11:19 +0100

    feat(library): display bookmarked items

--- src/app/library/library.page.ts
+++ src/app/library/library.page.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {Store} from '@ngrx/store';
+import {AppState} from '../store/app.reducer';
+import {Observable, Subject} from 'rxjs';
+import {Albums} from '../album.interface';
+import {map} from 'rxjs/operators';
+import values from 'lodash/values';
 
 @Component({
   selector: 'app-library',
@@ -6,10 +12,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./library.page.scss'],
 })
 export class LibraryPage implements OnInit {
+  items$: Observable<Albums>;
 
-  constructor() { }
+  constructor(private store: Store<{ app: AppState}>) {}
 
   ngOnInit() {
+    this.items$ = this.store
+        .select(state => state.app.library)
+        .pipe(map(obj => values(obj)));
   }
 
 }
